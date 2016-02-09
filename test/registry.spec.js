cf1338ca50984a9d47479c913ceb4e1f02634a2e
LeReunionais <reunionaisle@gmail.com>
2016-02-09 09:18:06 +0800

    Service name are now unique

--- test/registry.spec.js
+++ test/registry.spec.js
@@ -10,4 +10,22 @@ describe('register', () => {
       registry.registry().should.be.empty();
     })
   })
+  describe('service with same name', () => {
+    it('should replace the last one added', () => {
+      registry.register({
+        name: "name",
+        hostname: "localhost",
+        port: 3000,
+        protocol: "youpi"
+      })
+      registry.registry().should.have.length(1);
+      registry.register({
+        name: "name",
+        hostname: "localhost",
+        port: 3000,
+        protocol: "youpi"
+      })
+      registry.registry().should.have.length(1);
+    })
+  })
 })
